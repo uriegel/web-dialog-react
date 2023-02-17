6b016f2adcc7b055f6a2ce78939745d4c58ce514
Uwe Riegel <uriegel@hotmail.de>
2023-02-17 14:52:58 +0100

    no dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import './WebDialog.css';
-interface WebDialogProp {
-}
-declare const WebDialog: ({}: WebDialogProp) => JSX.Element;
-export default WebDialog;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './WebDialog.css';
-const WebDialog = ({}) => {
-    return (React.createElement("div", null, "Hello Web Dialog"));
-};
-export default WebDialog;
